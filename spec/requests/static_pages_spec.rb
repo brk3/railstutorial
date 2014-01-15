8ca2d13ad1952a37fdb3ff931ba01b9838203940
Paul Bourke <pauldbourke@gmail.com>
2014-01-15 22:48:25 +0000

    5.3.4 Pithy rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,52 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
   subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the right title" do
-      expect(page).to have_title("#{base_title} | About")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it "should have the content 'Contact Us'" do
-      expect(page).to have_content('Contact Us')
-    end
-
-    it "should have the right title" do
-      expect(page).to have_title("#{base_title} | Contact Us")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
-
 end
