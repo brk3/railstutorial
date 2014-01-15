9635ff68d6fe9d3c9fac6b1076d60fff0055079d
Paul Bourke <pauldbourke@gmail.com>
2014-01-15 23:04:16 +0000

    5.4.1 Add new users controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
-  root  'static_pages#home'
+  get "users/new"
 
+  root  'static_pages#home'
   get '/help', to: 'static_pages#help', as: 'help'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/signup', to: 'users#new', as: 'signup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
